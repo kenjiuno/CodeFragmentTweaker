b8911a56cb77536dc01bcbefa0b3742341519fc4
kenjiuno <h8mastre@gmail.com>
2024-01-01 20:45:16 +0900

    Re

--- CodeFragmentTweaker/makeDocs.bat
+++ CodeFragmentTweaker/makeDocs.bat
@@ -1,1 +1,1 @@
-dotnet publish -o publish -c:Release -p:GHPages=true -p:GHPagesBase=/SetPdfZipPassword/ && robocopy /e publish\wwwroot ..\docs
+dotnet publish -o publish -c:Release -p:GHPages=true -p:GHPagesBase=/CodeFragmentTweaker/ && robocopy /e publish\wwwroot ..\docs
